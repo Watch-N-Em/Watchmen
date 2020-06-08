5ee858673e05758ae8d411a92e1409a74ce26a75
PCwik88 <paul@cwik.us>
2020-06-08 17:49:57 -0500

    ok foundation is legit now

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="brand-name">
+          wATCHN EM
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{
+          width: "80%",
+          margin: "60px auto",
+        }}>
           We are currently under construction!  Check back again soon.  Updated regularly
         </p>
       </header>
